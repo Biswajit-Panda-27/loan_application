5ad7c9e0537a41a9bde96195334a5163b3c4a4f4
Biswajit-Panda-27 <biswajitpanda94389@gmail.com>
2024-09-16 22:33:37 +0530

    loan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoanApplicationComponent } from './pages/loan-application/loan-application.component';
+import { ApplicatinListComponent } from './pages/applicatin-list/applicatin-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoanApplicationComponent,ApplicatinListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoanApplicationComponent } from './pages/loan-application/loan-application.component';
+import { ApplicatinListComponent } from './pages/applicatin-list/applicatin-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'newApplication',
+        pathMatch: 'full'
+    },
+    {
+        path: 'newApplication',
+        component:LoanApplicationComponent
+    },
+    {
+        path: 'applicationList',
+        component:ApplicatinListComponent
+    }
+];
